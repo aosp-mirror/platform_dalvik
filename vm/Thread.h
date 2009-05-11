39b99b0038fcca0dfe972e70b182b559404ffc78
Andy McFadden <fadden@android.com>
2009-05-11 14:39:13 -0700

    Increase default stack size from 8K to 12K.
    For internal bug 1844105.

--- vm/Thread.h
+++ vm/Thread.h
@@ -72,7 +72,7 @@ void dvmSlayDaemons(void);
 #define kInternalRefMax         4096    /* mainly a sanity check */
 
 #define kMinStackSize       (512 + STACK_OVERFLOW_RESERVE)
-#define kDefaultStackSize   (8*1024)    /* two 4K pages */
+#define kDefaultStackSize   (12*1024)   /* three 4K pages */
 #define kMaxStackSize       (256*1024 + STACK_OVERFLOW_RESERVE)
 
 /*
