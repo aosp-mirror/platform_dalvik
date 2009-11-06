b1218ab315d66f75fdedf1eb98b51e82c73624f0
Dan Bornstein <danfuzz@android.com>
2009-11-05 16:53:48 -0800

    Clean up sieb.[ch], including changing a reference to <malloc.h> to instead be the more standard <stdlib.h>.
    I have no idea what "sieb" means (stands for?), and it looks like these
    files may want to be placed in the nearest recycling receptacle, but I'm
    not ready to go there today.
    
    Change-Id: I2d4116b742e61018fdbea127070154545ff82dbd

--- libcore/archive/src/main/native/sieb.c
+++ libcore/archive/src/main/native/sieb.c
@@ -1,38 +1,52 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "sieb.h"
 #include "JNIHelp.h"
 #include "jni.h"
 
-#include <malloc.h>
+#include <stdlib.h>
 
 // Throw java.lang.OutOfMemoryError
-void throwNewOutOfMemoryError (JNIEnv * env, const char *message)
-{
+void throwNewOutOfMemoryError(JNIEnv *env, const char *message) {
     jniThrowException(env, "java/lang/OutOfMemoryError", message);
 }
 
-void * sieb_malloc (JNIEnv * env, size_t byteCnt) {
-    void * adr = malloc(byteCnt);
+void *sieb_malloc(JNIEnv *env, size_t byteCnt) {
+    void *adr = malloc(byteCnt);
     if (adr == 0) {
-         if (byteCnt == 0)
-             throwNewOutOfMemoryError(env, "sieb_malloc(0) NOT ALLOWED");
-         else
-             throwNewOutOfMemoryError(env, "sieb_malloc");
+        if (byteCnt == 0) {
+            throwNewOutOfMemoryError(env, "sieb_malloc(0) NOT ALLOWED");
+        } else {
+            throwNewOutOfMemoryError(env, "sieb_malloc");
+        }
     }
     return adr;
 }
 
-void sieb_free (JNIEnv * env, void * adr) {
+void sieb_free(JNIEnv *env, void *adr) {
     free(adr);
 }
 
-
-
-void sieb_convertToPlatform (char *path) {
+void sieb_convertToPlatform(char *path) {
     char *pathIndex;
 
     pathIndex = path;
     while (*pathIndex != '\0') {
-        if(*pathIndex == '\\') {
+        if (*pathIndex == '\\') {
             *pathIndex = '/';
         }
         pathIndex++;
--- libcore/archive/src/main/native/sieb.h
+++ libcore/archive/src/main/native/sieb.h
@@ -1,21 +1,28 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #if !defined(sieb_h)
 #define sieb_h
-
 
 #include "JNIHelp.h"
 #include "jni.h"
 
-
-
-void throwNewOutOfMemoryError (JNIEnv * env,
-                               const char *message);
-
-
-void * sieb_malloc (JNIEnv * env, size_t byteCnt);
-void sieb_free (JNIEnv * env, void * adr);
-
-void sieb_convertToPlatform (char *path);
-
-
+void throwNewOutOfMemoryError(JNIEnv *env, const char *message);
+void *sieb_malloc(JNIEnv *env, size_t byteCnt);
+void sieb_free(JNIEnv *env, void *adr);
+void sieb_convertToPlatform(char *path);
 
 #endif /* sieb_h */
