8ba2708ea118381f2df5ca55b9bad2ae4c050504
Andy McFadden <fadden@android.com>
2010-05-21 12:20:23 -0700

    Added EXPORT_PC to "throw" instruction.
    For bug 2700761.
    
    Change-Id: I889e59ea35d9cadd99fc884e5b1301a4cf103f93

--- vm/mterp/out/InterpC-portstd.c
+++ vm/mterp/out/InterpC-portstd.c
@@ -1914,10 +1914,17 @@ HANDLE_OPCODE(OP_THROW /*vAA*/)
     {
         Object* obj;
 
+        /*
+         * We don't create an exception here, but the process of searching
+         * for a catch block can do class lookups and throw exceptions.
+         * We need to update the saved PC.
+         */
+        EXPORT_PC();
+
         vsrc1 = INST_AA(inst);
         ILOGV("|throw v%d  (%p)", vsrc1, (void*)GET_REGISTER(vsrc1));
         obj = (Object*) GET_REGISTER(vsrc1);
-        if (!checkForNullExportPC(obj, fp, pc)) {
+        if (!checkForNull(obj)) {
             /* will throw a null pointer exception */
             LOGVV("Bad exception\n");
         } else {
