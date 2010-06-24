ec022295682131fd8ebd4619343060fd3554319b
Dan Bornstein <danfuzz@android.com>
2010-06-24 14:51:17 -0700

    Updated the warning about old-style inner class metadata to reflect the current understanding of the situation.
    The change affects diagnostic output but doesn't change anything about
    dex file production, per se. I also added a new test each for dx and
    the vm, checking that the warning gets emitted and demonstrating the
    effect (respectively).
    
    Though it's a pretty minor change, I bumped up the version number of dx.
    
    Added bonus: Wrapped a couple of too-long lines that I happened to notice.
    
    Bug: 2171430
    Change-Id: If6afa4ba88dca9b0e6d88603e0a5fc2e36358115

--- dx/tests/113-old-style-inner-class/Blort.java
+++ dx/tests/113-old-style-inner-class/Blort.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (C) 2010 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,14 @@
  * limitations under the License.
  */
 
-package com.android.dx;
+public class Blort {
+    public static Runnable theRunnable = new Runnable() {
+            public void run() { }
+        };
 
-/**
- * Version number for dx.
- */
-public class Version {
-    /** {@code non-null;} version string */
-    public static final String VERSION = "1.5";
+    public Runnable create() {
+        return new Runnable() {
+                public void run() { }
+            };
+    }
 }
