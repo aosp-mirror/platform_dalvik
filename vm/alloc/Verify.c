ba56c323c4dbaea792c65539ea27d9e279805500
Carl Shapiro <cshapiro@google.com>
2010-10-18 17:36:13 -0700

    Remove an obsolete debugging message.
    Change-Id: Idb6af743bdd2e60a64d8ba8488d373f1724be3a2

--- vm/alloc/Verify.c
+++ vm/alloc/Verify.c
@@ -33,7 +33,6 @@ static void dumpReferencesCallback(void *ptr, void *arg)
 {
     Object *obj = arg;
     if (ptr == obj) {
-        LOGD("skipping %p == %p", ptr, obj);
         return;
     }
     dvmVisitObject(dumpReferencesVisitor, ptr, &obj);
