949c3ec207a7720fb47f7b3ca1f84dfcfd70aaa9
Jeff Brown <jeffbrown@google.com>
2011-06-24 17:05:33 -0700

    Add a dual clock profiler tracing mode.
    Dalvik previously supported using either the thread-cpu clock
    or the real time clock as the timebase for profiler traces.
    This change adds a dual clock mode where both thread-cpu time
    and real time timestamps are collected.
    
    Using dual clock mode significantly improves TraceView ability
    to accurately reconstruct the global timeline of events,
    particularly on SMP systems.
    
    For now, thread-cpu mode remains the default.
    
    Dual clock mode can be enabled by running the following command
    and restarting the system server.
       adb shell setprop dalvik.vm.extra-opts -Xprofile:dualclock
    
    Change-Id: I8c0d91a99aa6829dadea328e54dc1225d9827391

--- vm/Init.cpp
+++ vm/Init.cpp
@@ -1010,8 +1010,12 @@ static int processOptions(int argc, const char* const argv[],
         } else if (strcmp(argv[i], "-Xcheckdexsum") == 0) {
             gDvm.verifyDexChecksum = true;
 
+        } else if (strcmp(argv[i], "-Xprofile:threadcpuclock") == 0) {
+            gDvm.profilerClockSource = kProfilerClockSourceThreadCpu;
         } else if (strcmp(argv[i], "-Xprofile:wallclock") == 0) {
-            gDvm.profilerWallClock = true;
+            gDvm.profilerClockSource = kProfilerClockSourceWall;
+        } else if (strcmp(argv[i], "-Xprofile:dualclock") == 0) {
+            gDvm.profilerClockSource = kProfilerClockSourceDual;
 
         } else {
             if (!ignoreUnrecognized) {
@@ -1086,6 +1090,11 @@ static void setCommandLineDefaults()
      * dexopt target a differently-configured device.
      */
     gDvm.dexOptForSmp = (ANDROID_SMP != 0);
+
+    /*
+     * Default profiler configuration.
+     */
+    gDvm.profilerClockSource = kProfilerClockSourceThreadCpu;
 }
 
 
