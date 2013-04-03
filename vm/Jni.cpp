8bd996ebafb147d0e4b3483cbbb0e2e8a6ab9171
You Kim <you.kim72@gmail.com>
2013-04-04 03:58:11 +0900

    Fix minor leaks caused by failed initialization in JNI_CreateJavaVM
    Change-Id: I97e4b162fa729c97a1e71786b696f2befed56835

--- vm/Jni.cpp
+++ vm/Jni.cpp
@@ -3502,6 +3502,8 @@ jint JNI_CreateJavaVM(JavaVM** p_vm, JNIEnv** p_env, void* vm_args) {
                 } else {
                     dvmFprintf(stderr, "ERROR: CreateJavaVM failed: unknown -Xjniopts option '%s'\n",
                             jniOpt);
+                    free(pVM);
+                    free(jniOpts);
                     return JNI_ERR;
                 }
                 jniOpt += strlen(jniOpt) + 1;
@@ -3519,6 +3521,7 @@ jint JNI_CreateJavaVM(JavaVM** p_vm, JNIEnv** p_env, void* vm_args) {
 
     if (gDvmJni.jniVm != NULL) {
         dvmFprintf(stderr, "ERROR: Dalvik only supports one VM per process\n");
+        free(pVM);
         return JNI_ERR;
     }
     gDvmJni.jniVm = (JavaVM*) pVM;
