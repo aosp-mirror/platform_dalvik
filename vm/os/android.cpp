dc0fe3850ed69a5deaff53645a91daa94741b7cc
Alex Ray <aray@google.com>
2013-07-23 17:17:50 -0700

    libdvm: move thread definitions dependency
    Change-Id: I88351f5b1a8bd2dccab8ab86fd400ec707f5809a

--- vm/os/android.cpp
+++ vm/os/android.cpp
@@ -23,8 +23,8 @@
 #include <limits.h>
 #include <errno.h>
 
+#include <system/thread_defs.h>
 #include <cutils/sched_policy.h>
-#include <utils/threads.h>
 
 /*
  * Conversion map for "nice" values.
