c00fafdb91760a4cdfbe9e5323b5165ab4c01b76
Benoit Lamarche <benoitlamarche@google.com>
2014-02-28 16:01:21 +0100

    Fix detection of main dex overflow in multidex
    Bug: 13239620
    
    Change-Id: Ie897070cbbe0605f38fd6db7b6a50c91f2d1364b

--- dx/src/com/android/dx/command/dexer/Main.java
+++ dx/src/com/android/dx/command/dexer/Main.java
@@ -476,7 +476,7 @@ public class Main {
                     }
                 }
 
-                if (dexOutputArrays.size() > 1) {
+                if (dexOutputArrays.size() > 0) {
                     throw new DexException("Too many classes in " + Arguments.MAIN_DEX_LIST_OPTION
                             + ", main dex capacity exceeded");
                 }
