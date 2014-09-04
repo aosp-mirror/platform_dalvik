4de3efd5e674f26b3f45fbb0df9a1772948c8785
Ying Wang <wangying@google.com>
2014-09-04 13:14:45 -0700

    Add libcore src to the dx docs module.
    This fixed bogus docs build errors like:
    dalvik/dx/src/com/android/dx/io/DexIndexPrinter.java:19: error: package
    com.android.dex does not exist
    
    Change-Id: I3217a51c1562194974410c54f936c0713455a93c

--- dx/src/Android.mk
+++ dx/src/Android.mk
@@ -5,12 +5,15 @@ LOCAL_PATH := $(call my-dir)
 # This tool is prebuilt if we're doing an app-only build.
 ifeq ($(TARGET_BUILD_APPS)$(filter true,$(TARGET_BUILD_PDK)),)
 
+dx_src_files := \
+  $(call all-subdir-java-files) \
+  $(call all-java-files-under,../../../libcore/dex/src/main/java)
+
 # dx java library
 # ============================================================
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_SRC_FILES += $(call all-java-files-under,../../../libcore/dex/src/main/java)
+LOCAL_SRC_FILES := $(dx_src_files)
 LOCAL_JAR_MANIFEST := ../etc/manifest.txt
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE:= dx
@@ -29,7 +32,7 @@ endif # No TARGET_BUILD_APPS or TARGET_BUILD_PDK
 # ============================================================
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-subdir-html-files)
+LOCAL_SRC_FILES := $(dx_src_files) $(call all-subdir-html-files)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE:= dx
 LOCAL_DROIDDOC_OPTIONS := -hidden
@@ -37,3 +40,5 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_IS_HOST_MODULE := true
 
 include $(BUILD_DROIDDOC)
+
+dx_src_files :=
